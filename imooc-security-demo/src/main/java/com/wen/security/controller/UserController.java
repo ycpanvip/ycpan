1ba7010c5b9dd378ea37cc18a2f6fa9d303ce4bc
haha174 <cwqxhxh@163.com>
2017-10-10 18:08:52 +0800

    test

--- imooc-security-demo/src/main/java/com/wen/security/controller/UserController.java
+++ imooc-security-demo/src/main/java/com/wen/security/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.wen.security.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wen.security.model.User;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	@GetMapping("/user.do")
-	public  String hello() {
-		return "hello world";
+	@GetMapping
+	public  List<User> hello(@RequestParam(value="username",required=true,defaultValue="cxhc") String name) {
+		System.out.println(name);
+		List<User> list=new ArrayList<User>();
+		list.add(new  User());
+		list.add(new  User());
+		list.add(new  User());
+		return list;
 	}
 }
